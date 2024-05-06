3d491e8ba574944c5437b3a29bbe13dd473bc388
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-06 17:30:11 -0300

    feat: ensure a CPF is not create with success

--- src/core/domain/value-objects/Cpf.ts
+++ src/core/domain/value-objects/Cpf.ts
@@ -1,3 +1,7 @@
+import { assertArgumentLength } from '../base/AssertionConcerns'
+
 export class CPF {
-  constructor (readonly value: string) {}
+  constructor (readonly value: string) {
+    assertArgumentLength(value, 11, 11, 'Cpf must have only number and 11 characteres!')
+  }
 }
