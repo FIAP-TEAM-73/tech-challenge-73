42c6c68e7ca8dc5915c3f01e1902b9c2a743ad64
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-06 16:55:32 -0300

    test: ensure that a Customer is not create with success when FirstName is out of the min length

--- test/core/domain/entities/Customer.spec.ts
+++ test/core/domain/entities/Customer.spec.ts
@@ -1,3 +1,4 @@
+import { DomainError } from '../../../../src/core/domain/base/DomainError'
 import { Customer } from '../../../../src/core/domain/entities/Customer'
 
 describe('Create a Customer', () => {
@@ -6,5 +7,9 @@ describe('Create a Customer', () => {
     expect(sut.name).toBe('first_name last_name')
     expect(sut.phone).toBe('35999111115')
     expect(sut.cpf).toBe('12559757680')
+  })
+  it('Should create a Customer with error when firstName is not provided', () => {
+    expect(() => new Customer('', 'last_name', '35999111115', '12559757680'))
+      .toThrow(new DomainError('First name must be greater than 3 and less than 50!'))
   })
 })
