47fede7a8596eb1035b1f1dbb1e0eae63210f7da
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-06 17:00:16 -0300

    refactor: removing code smells

--- src/core/domain/entities/Customer.ts
+++ src/core/domain/entities/Customer.ts
@@ -1,14 +1,11 @@
 import { assertArgumentLength } from '../base/AssertionConcerns'
 
 export class Customer {
-  readonly name
   constructor (
-    firstName: string,
-    lastName: string,
+    readonly name: string,
     readonly phone: string,
     readonly cpf: string
   ) {
-    assertArgumentLength(firstName, 3, 50, 'First name must be greater than 3 and less than 50!')
-    this.name = `${firstName} ${lastName}`
+    assertArgumentLength(name, 3, 100, 'Name must be greater than 3 and less than 100!')
   }
 }
--- test/core/domain/entities/Customer.spec.ts
+++ test/core/domain/entities/Customer.spec.ts
@@ -1,15 +1,21 @@
 import { DomainError } from '../../../../src/core/domain/base/DomainError'
 import { Customer } from '../../../../src/core/domain/entities/Customer'
 
+const mockCustomer = {
+  name: 'Customer Name',
+  phone: '35999111115',
+  cpf: '12559757680'
+}
+
 describe('Create a Customer', () => {
   it('Should create a Customer with success when all attributes are correct', () => {
-    const sut = new Customer('first_name', 'last_name', '35999111115', '12559757680')
-    expect(sut.name).toBe('first_name last_name')
-    expect(sut.phone).toBe('35999111115')
-    expect(sut.cpf).toBe('12559757680')
+    const sut = new Customer(mockCustomer.name, mockCustomer.phone, mockCustomer.cpf)
+    expect(sut.name).toBe(mockCustomer.name)
+    expect(sut.phone).toBe(mockCustomer.phone)
+    expect(sut.cpf).toBe(mockCustomer.cpf)
   })
-  it('Should create a Customer with error when firstName is not provided', () => {
-    expect(() => new Customer('', 'last_name', '35999111115', '12559757680'))
-      .toThrow(new DomainError('First name must be greater than 3 and less than 50!'))
+  it('Should create a Customer with error when name is not provided', () => {
+    expect(() => new Customer('', mockCustomer.phone, mockCustomer.cpf))
+      .toThrow(new DomainError('Name must be greater than 3 and less than 100!'))
   })
 })
