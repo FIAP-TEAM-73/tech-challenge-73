e41145b9e16a6736c2e3b2f89357cef93762fea5
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-06 17:37:24 -0300

    test: ensure a Phone is not created with success

--- test/core/domain/value-objects/Phone.spec.ts
+++ test/core/domain/value-objects/Phone.spec.ts
@@ -1,8 +1,12 @@
+import { DomainError } from '../../../../src/core/domain/base/DomainError'
 import { Phone } from '../../../../src/core/domain/value-objects/Phone'
 
 describe('Create a phone', () => {
   it('Should create a phone with success when its value is correct', () => {
-    const sut = new Phone('any')
-    expect(sut.value).toBe('any')
+    const sut = new Phone('35999111115')
+    expect(sut.value).toBe('35999111115')
+  })
+  it('Should not create a phone with success when its value is incorrect', () => {
+    expect(() => new Phone('')).toThrow(new DomainError('Phone must have only numbers and 11 characteres!'))
   })
 })
