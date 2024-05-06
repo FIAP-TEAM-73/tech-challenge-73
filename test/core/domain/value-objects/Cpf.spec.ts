8597769bc2972fe476da502dc2b3fa549b5172b1
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-06 17:30:28 -0300

    test: ensure a CPF is not create with success

--- test/core/domain/value-objects/Cpf.spec.ts
+++ test/core/domain/value-objects/Cpf.spec.ts
@@ -1,8 +1,12 @@
+import { DomainError } from '../../../../src/core/domain/base/DomainError'
 import { CPF } from '../../../../src/core/domain/value-objects/Cpf'
 
 describe('Create a CPF', () => {
   it('Should create a CPF when its value is valid', () => {
-    const sut = new CPF('any')
-    expect(sut.value).toBe('any')
+    const sut = new CPF('48720041241')
+    expect(sut.value).toBe('48720041241')
+  })
+  it('Should not create a CPF when its value is invalid', () => {
+    expect(() => new CPF('')).toThrow(new DomainError('Cpf must have only number and 11 characteres!'))
   })
 })
