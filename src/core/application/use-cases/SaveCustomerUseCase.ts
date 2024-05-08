46d96c4ee324f02e03ed54ed46e646f589ce7f21
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-08 00:15:44 -0300

    feat: ensure SaveCustomerUseCase save a customer with success

---
+++ src/core/application/use-cases/SaveCustomerUseCase.ts
@@ -0,0 +1,19 @@
+import { Customer } from '../../domain/entities/Customer'
+import { type ICustomerRepository } from '../../domain/repositories/ICustomerRepository'
+import { CPF } from '../../domain/value-objects/Cpf'
+import { Phone } from '../../domain/value-objects/Phone'
+
+export interface CustomerRequest {
+  name: string
+  phone: string
+  cpf: string
+}
+
+export class SaveCustomerUseCase {
+  constructor (private readonly customerRepository: ICustomerRepository) {}
+
+  async execute (request: CustomerRequest): Promise<string> {
+    const customer = new Customer('1', request.name, new Phone(request.phone), new CPF(request.cpf))
+    return await this.customerRepository.save(customer)
+  }
+}
