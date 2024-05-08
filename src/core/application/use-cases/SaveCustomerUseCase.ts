d755b9d47b5d5b24878527c64e2d7274f4ba9c84
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-08 00:39:00 -0300

    feat: adding uuid to Customer creation

--- src/core/application/use-cases/SaveCustomerUseCase.ts
+++ src/core/application/use-cases/SaveCustomerUseCase.ts
@@ -2,7 +2,7 @@ import { Customer } from '../../domain/entities/Customer'
 import { type ICustomerRepository } from '../../domain/repositories/ICustomerRepository'
 import { CPF } from '../../domain/value-objects/Cpf'
 import { Phone } from '../../domain/value-objects/Phone'
-
+import { v4 as uuidv4 } from 'uuid'
 export interface CustomerRequest {
   name: string
   phone: string
@@ -14,7 +14,7 @@ export class SaveCustomerUseCase {
 
   async execute (request: CustomerRequest): Promise<string> {
     try {
-      const customer = new Customer('1', request.name, new Phone(request.phone), new CPF(request.cpf))
+      const customer = new Customer(uuidv4(), request.name, new Phone(request.phone), new CPF(request.cpf))
       return await this.customerRepository.save(customer)
     } catch (error) {
       throw new Error('Fail while saving a customer.')
--- test/core/application/use-cases/SaveCustomerUseCase.spec.ts
+++ test/core/application/use-cases/SaveCustomerUseCase.spec.ts
@@ -1,6 +1,9 @@
 import { SaveCustomerUseCase, type CustomerRequest } from '../../../../src/core/application/use-cases/SaveCustomerUseCase'
 import { DomainError } from '../../../../src/core/domain/base/DomainError'
 import { type ICustomerRepository } from '../../../../src/core/domain/repositories/ICustomerRepository'
+import * as uuid from 'uuid'
+
+jest.mock('uuid')
 
 describe('Save Customer use case', () => {
   const mockCustomerRepository: ICustomerRepository = {
@@ -8,6 +11,7 @@ describe('Save Customer use case', () => {
   }
 
   it('Should create a customer with success', async () => {
+    jest.spyOn(uuid, 'v4').mockReturnValueOnce('mocked_id')
     const sut = new SaveCustomerUseCase(mockCustomerRepository)
     const customerRequest: CustomerRequest = {
       name: 'Any Customer Name',
@@ -15,7 +19,7 @@ describe('Save Customer use case', () => {
       cpf: '12559757619'
     }
     const result = await sut.execute(customerRequest)
-    expect(result).toBe('1')
+    expect(result).toBe('mocked_id')
   })
   it('Should throw when repository throws', async () => {
     const mockCustomerRepository: ICustomerRepository = {
