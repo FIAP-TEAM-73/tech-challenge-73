bea3b45eeaebf18c8766f55219196e826d408203
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-08 00:16:00 -0300

    test: ensure SaveCustomerUseCase save a customer with success

---
+++ test/core/application/use-cases/SaveCustomerUseCase.spec.ts
@@ -0,0 +1,19 @@
+import { SaveCustomerUseCase, type CustomerRequest } from '../../../../src/core/application/use-cases/SaveCustomerUseCase'
+import { type ICustomerRepository } from '../../../../src/core/domain/repositories/ICustomerRepository'
+
+describe('Save Customer use case', () => {
+  const mockCustomerRepository: ICustomerRepository = {
+    save: jest.fn(async (customer) => await Promise.resolve(customer.id))
+  }
+
+  it('Should create a customer with success', async () => {
+    const sut = new SaveCustomerUseCase(mockCustomerRepository)
+    const customerRequest: CustomerRequest = {
+      name: 'Any Customer Name',
+      phone: '35999251111',
+      cpf: '12559757619'
+    }
+    const result = await sut.execute(customerRequest)
+    expect(result).toBe('1')
+  })
+})
