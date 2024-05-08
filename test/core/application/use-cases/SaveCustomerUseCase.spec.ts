6ebff92d13a4e5a69a79b52bd49f271e98e0a396
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-08 00:32:09 -0300

    test: ensure SaveCustomerUseCase throws when repository return an error

--- test/core/application/use-cases/SaveCustomerUseCase.spec.ts
+++ test/core/application/use-cases/SaveCustomerUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { SaveCustomerUseCase, type CustomerRequest } from '../../../../src/core/application/use-cases/SaveCustomerUseCase'
+import { DomainError } from '../../../../src/core/domain/base/DomainError'
 import { type ICustomerRepository } from '../../../../src/core/domain/repositories/ICustomerRepository'
 
 describe('Save Customer use case', () => {
@@ -15,5 +16,18 @@ describe('Save Customer use case', () => {
     }
     const result = await sut.execute(customerRequest)
     expect(result).toBe('1')
+  })
+  it('Should throw when repository throws', async () => {
+    const mockCustomerRepository: ICustomerRepository = {
+      save: jest.fn(async () => await Promise.reject(new DomainError('Generic repository error')))
+    }
+    const sut = new SaveCustomerUseCase(mockCustomerRepository)
+    const customerRequest: CustomerRequest = {
+      name: 'Any Customer Name',
+      phone: '35999251111',
+      cpf: '12559757619'
+    }
+    const result = sut.execute(customerRequest)
+    await expect(result).rejects.toEqual(new Error('Fail while saving a customer.'))
   })
 })
