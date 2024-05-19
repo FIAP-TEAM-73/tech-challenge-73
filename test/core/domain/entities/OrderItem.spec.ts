de14a9c93ad66eceb1da7dbd2784bc8b9180277e
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-19 10:57:46 -0300

    test: ensure OrderItem calculate total price

---
+++ test/core/domain/entities/OrderItem.spec.ts
@@ -0,0 +1,11 @@
+import OrderItem from '../../../../src/core/domain/entities/OrderItem'
+
+describe('Calculate OrderItem total value', () => {
+  it('Should calculate the total price when the OrderItem is valid', () => {
+    const itemPrice = 30.00
+    const itemQuantity = 2
+    const sut = new OrderItem('any_item_id', 'any_order_id', itemPrice, itemQuantity)
+    const totalPrice = sut.calculateTotal()
+    expect(totalPrice).toBe(60)
+  })
+})
