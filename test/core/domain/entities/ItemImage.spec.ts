1ed8d40fc658d474e4984a5ed03a8b8efaa67869
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-24 19:10:45 -0300

    test: ensure ItemImage is create with success when storagePath is correctly

--- test/core/domain/entities/ItemImage.spec.ts
+++ test/core/domain/entities/ItemImage.spec.ts
@@ -19,4 +19,11 @@ describe('Create Item image', () => {
     expect(sut.base64).toBe('any_base64')
     expect(sut.storagePath).toBeUndefined()
   })
+  it('Should create an item when base64 is valid', () => {
+    const sut = new ItemImage('any_item_image_id', 'any_item_id', undefined, 'any_storage_path')
+    expect(sut.id).toBe('any_item_image_id')
+    expect(sut.itemId).toBe('any_item_id')
+    expect(sut.storagePath).toBe('any_storage_path')
+    expect(sut.base64).toBeUndefined()
+  })
 })
