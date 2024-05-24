7fbe1bb757a01f80d9b7198a8d3e1ba23a3d1e34
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-24 18:02:20 -0300

    feat: ensure Item is not created when item category does not exist

--- src/core/domain/entities/Item.ts
+++ src/core/domain/entities/Item.ts
@@ -1,14 +1,17 @@
-import { assertArgumentLength } from '../base/AssertionConcerns'
+import { assertArgumentLength, assertArgumentUnionType } from '../base/AssertionConcerns'
 
+const categories = ['BURGERS', 'SIDES', 'DRINKS', 'DESSERTS'] as const
+export type ItemCategory = (typeof categories)[number]
 export default class Item {
   constructor (
     readonly id: string,
     readonly name: string,
-    readonly category: string,
+    readonly category: ItemCategory,
     readonly price: number,
     readonly description: string,
     readonly pathImages: string[]
   ) {
     assertArgumentLength(name, 3, 100, 'Name must be greater than 3 and less than 100!')
+    assertArgumentUnionType(category, Object.values(categories), `Category '${category}' is invalid!`)
   }
 }
