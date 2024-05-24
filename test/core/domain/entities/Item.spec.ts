41767a16af8e747b7904caed07b51c730185c718
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-24 18:02:37 -0300

    test: ensure Item is not created when item category does not exist

--- test/core/domain/entities/Item.spec.ts
+++ test/core/domain/entities/Item.spec.ts
@@ -1,18 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { DomainError } from '../../../../src/core/domain/base/DomainError'
 import Item from '../../../../src/core/domain/entities/Item'
 
 describe('Create an Item', () => {
   it('Should create an Item when every attribute is right', () => {
-    const sut = new Item('ítem_id', 'any item name', 'burguer', 35.0, 'any item description', ['any image path'])
+    const sut = new Item('ítem_id', 'any item name', 'BURGERS', 35.0, 'any item description', ['any image path'])
     expect(sut.id).toBe('ítem_id')
     expect(sut.name).toBe('any item name')
-    expect(sut.category).toBe('burguer')
+    expect(sut.category).toBe('BURGERS')
     expect(sut.price).toBe(35.0)
     expect(sut.description).toBe('any item description')
     expect(sut.pathImages).toEqual(['any image path'])
   })
   it('Should not create an Item when name is invalid', () => {
-    expect(() => new Item('ítem_id', '', 'burguer', 35.0, 'any item description', ['any image path']))
+    expect(() => new Item('ítem_id', '', 'BURGERS', 35.0, 'any item description', ['any image path']))
       .toThrow(new DomainError('Name must be greater than 3 and less than 100!'))
+  })
+  it('Should not create an Item when category does not exist', () => {
+    expect(() => new Item('ítem_id', 'any item name', 'WRONG_MEAL' as any, 35.0, 'any item description', ['any image path']))
+      .toThrow(new DomainError('Category \'WRONG_MEAL\' is invalid!'))
   })
 })
