3b66f522db852cbd7d0af60b85a5ffbb294bb5c0
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-24 18:10:48 -0300

    feat: ensure Item is not created when description is invalid

--- src/core/domain/entities/Item.ts
+++ src/core/domain/entities/Item.ts
@@ -14,5 +14,6 @@ export default class Item {
     assertArgumentLength(name, 3, 100, 'Name must be greater than 3 and less than 100!')
     assertArgumentUnionType(category, Object.values(categories), `Category '${category}' is invalid!`)
     assertArgumentMin(price, 0, 'Price must be greater than 0')
+    assertArgumentLength(description, 3, 255, 'Description must be greater than 3 and less than 255!')
   }
 }
