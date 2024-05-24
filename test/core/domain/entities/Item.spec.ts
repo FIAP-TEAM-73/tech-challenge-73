e2e59a42a9cc25f8273e58bc111d0a9c7a2e62f0
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-24 17:41:53 -0300

    test: ensure Item is not created when name is wrong

--- test/core/domain/entities/Item.spec.ts
+++ test/core/domain/entities/Item.spec.ts
@@ -1,3 +1,4 @@
+import { DomainError } from '../../../../src/core/domain/base/DomainError'
 import Item from '../../../../src/core/domain/entities/Item'
 
 describe('Create an Item', () => {
@@ -9,5 +10,9 @@ describe('Create an Item', () => {
     expect(sut.price).toBe(35.0)
     expect(sut.description).toBe('any item description')
     expect(sut.pathImages).toEqual(['any image path'])
+  })
+  it('Should not create an Item when name is invalid', () => {
+    expect(() => new Item('ítem_id', '', 'burguer', 35.0, 'any item description', ['any image path']))
+      .toThrow(new DomainError('Name must be greater than 3 and less than 100!'))
   })
 })
