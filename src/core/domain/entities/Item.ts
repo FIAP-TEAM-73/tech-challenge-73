99fd7f1cca277371e637c128b9db183a90e10000
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-24 19:26:29 -0300

    feat: create ItemImage and adding it to Item object

--- src/core/domain/entities/Item.ts
+++ src/core/domain/entities/Item.ts
@@ -1,4 +1,5 @@
 import { assertArgumentLength, assertArgumentMin, assertArgumentUnionType } from '../base/AssertionConcerns'
+import type ItemImage from './ItemImage'
 
 const categories = ['BURGERS', 'SIDES', 'DRINKS', 'DESSERTS'] as const
 export type ItemCategory = (typeof categories)[number]
@@ -9,7 +10,7 @@ export default class Item {
     readonly category: ItemCategory,
     readonly price: number,
     readonly description: string,
-    readonly pathImages: string[]
+    readonly pathImages: ItemImage[]
   ) {
     assertArgumentLength(name, 3, 100, 'Name must be greater than 3 and less than 100!')
     assertArgumentUnionType(category, Object.values(categories), `Category '${category}' is invalid!`)
