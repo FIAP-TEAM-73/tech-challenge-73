59a7361ec2bae47d0d7737eb0cfaf6646eb2bdcf
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-24 19:48:02 -0300

    test: ensure ItemRepository save an Item with sucess

---
+++ test/adapter/driven/infra/repositories/ItemRepository.spec.ts
@@ -0,0 +1,34 @@
+import ItemRepository from '../../../../../src/adapter/driven/infra/repositories/ItemRepository'
+import type IConnection from '../../../../../src/core/domain/database/IConnection'
+import Item from '../../../../../src/core/domain/entities/Item'
+import ItemImage from '../../../../../src/core/domain/entities/ItemImage'
+
+const mockItemImage = [
+  new ItemImage('any_item_image_id', 'any_item_id', 'any_base64', undefined),
+  new ItemImage('another_item_image_id', 'any_item_id', 'any_base64', undefined)
+]
+const mockItem = new Item('ítem_id', 'any item name', 'BURGERS', 35.0, 'any item description', mockItemImage)
+
+describe('Item Repository', () => {
+  const mockConnection: IConnection = {
+    isAlive: async () => await Promise.resolve(true),
+    close: async () => { },
+    connect: async () => { },
+    query: async (stmt: string, params: any[]) => {
+      console.log({ stmt, params })
+      return await Promise.resolve({
+        rows: [
+          { id: 'item_id', name: 'any item name', category: 'BURGERS', price: 35.0, description: 'any item description', item_image_id: 'any_item_image_id', item_id: 'item_id', base64: 'any_base_64', storage_path: null },
+          { id: 'item_id', name: 'any item name', category: 'BURGERS', price: 35.0, description: 'any item description', item_image_id: 'another_item_image_id', item_id: 'item_id', base64: 'any_base_64', storage_path: null }
+        ]
+      })
+    }
+  }
+  describe('Create an Item', () => {
+    it('Should create a item with sucess when all attributes are correct', async () => {
+      const sut = new ItemRepository(mockConnection)
+      const result = await sut.save(mockItem)
+      expect(result).toBe('item_id')
+    })
+  })
+})
