15ad7bb4a24dbe2903f9a2e86cd64d2a87659af9
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-25 10:11:01 -0300

    feat: ensure ItemRepository return empty when any filter matches

--- src/adapter/driven/infra/repositories/ItemRepository.ts
+++ src/adapter/driven/infra/repositories/ItemRepository.ts
@@ -11,6 +11,7 @@ interface ItemRow {
   category: ItemCategory
   price: number
   description: string
+  is_active: boolean
   item_image_id: string
   base64: string
   storage_path: string
@@ -27,9 +28,9 @@ export default class ItemRepository implements IItemRepository {
   constructor (private readonly connection: IConnection) {}
 
   async save (item: Item): Promise<string> {
-    const query = 'INSERT INTO "item"(id, name, category, price, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT (id) DO UPDATE SET name=$2, category=$3, price=$4, description=$5 RETURNING *'
-    const { id, name, category, price, description, pathImages } = item
-    const values = [id, name, category, price, description]
+    const query = 'INSERT INTO "item"(id, name, category, price, description, is_active) VALUES($1, $2, $3, $4, $5, $6) ON CONFLICT (id) DO UPDATE SET name=$2, category=$3, price=$4, description=$5, is_active=$6 RETURNING *'
+    const { id, name, category, price, description, pathImages, isActive } = item
+    const values = [id, name, category, price, description, isActive]
     const result = await this.connection.query(query, values)
     const itemId: string = result.rows[0].id
     await this.saveItemImage(itemId, pathImages)
@@ -53,12 +54,12 @@ export default class ItemRepository implements IItemRepository {
     const result = await this.connection.query(query, [id])
     if (result.rows.length === 0) return undefined
     return result.rows.reduce((acc: Item | undefined, row: ItemRow) => {
-      const { id, name, category, description, price, item_image_id: itemImageId, base64, storage_path: storagePath } = row
+      const { id, name, category, description, is_active: isActive, price, item_image_id: itemImageId, base64, storage_path: storagePath } = row
       const itemImage = new ItemImage(itemImageId, id, base64, storagePath)
       if (acc === undefined) {
-        return new Item(id, name, category, price, description, [itemImage])
+        return new Item(id, name, category, price, description, [itemImage], isActive)
       }
-      return new Item(acc.id, acc.name, acc.category, acc.price, acc.description, [...acc.pathImages, itemImage])
+      return new Item(acc.id, acc.name, acc.category, acc.price, acc.description, [...acc.pathImages, itemImage], isActive)
     }, undefined)
   }
 
@@ -70,16 +71,17 @@ export default class ItemRepository implements IItemRepository {
     AND (name = $2 OR null = $2)
     AND (category = $3 OR null = $3)
     AND (price = $4 OR null = $4)
-    LIMIT $5
-    OFFSET $6
+    AND is_active = $5
+    LIMIT $6
+    OFFSET $7
     `
-    const { page, size, category, id, name, price } = params
-    const result = await this.connection.query(query, [id, name, category, price, size, (size * page)])
+    const { page, size, category, id, name, price, isActive = true } = params
+    const result = await this.connection.query(query, [id, name, category, price, isActive, size, (size * page)])
     if (result.rows.length === 0) return []
     return await Promise.all(result.rows.map(async (row: ItemRow) => {
-      const { id, name, category, description, price } = row
+      const { id, name, category, description, price, is_active: isActive } = row
       const pathImages = await this.findItemImageByItemId(id)
-      return new Item(id, name, category, price, description, pathImages)
+      return new Item(id, name, category, price, description, pathImages, isActive)
     }))
   }
 
