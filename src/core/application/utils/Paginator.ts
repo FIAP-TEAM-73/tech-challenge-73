7dc90a831654044954dce661dae6e1aafaa60505
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-25 14:26:33 -0300

    feat: ensure Paginator return totalPages with the correct value

--- src/core/application/utils/Paginator.ts
+++ src/core/application/utils/Paginator.ts
@@ -1,11 +1,16 @@
 export default class Paginator<T> {
   constructor (
     readonly content: T[],
+    readonly size: number,
     readonly page: number,
     readonly total: number
   ) {}
 
   isFirstPage (): boolean {
     return this.page === 0
+  }
+
+  getTotalPages (): number {
+    return Math.ceil(this.total / this.size)
   }
 }
