88fc1768f7e9d3cf83f4cf657c26b708b50359cf
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-24 21:30:16 -0300

    feat: ensure ItemRepository return an Item if exists

--- src/adapter/driven/infra/repositories/ItemRepository.ts
+++ src/adapter/driven/infra/repositories/ItemRepository.ts
@@ -1,8 +1,19 @@
 import type IConnection from '../../../../core/domain/database/IConnection'
-import type Item from '../../../../core/domain/entities/Item'
-import type ItemImage from '../../../../core/domain/entities/ItemImage'
+import Item from '../../../../core/domain/entities/Item'
+import { type ItemCategory } from '../../../../core/domain/entities/Item'
+import ItemImage from '../../../../core/domain/entities/ItemImage'
 import type IItemRepository from '../../../../core/domain/repositories/IItemRepository'
 
+interface ItemRow {
+  id: string
+  name: string
+  category: ItemCategory
+  price: number
+  description: string
+  item_image_id: string
+  base64: string
+  storage_path: string
+}
 export default class ItemRepository implements IItemRepository {
   constructor (private readonly connection: IConnection) {}
   async save (item: Item): Promise<string> {
@@ -16,10 +27,28 @@ export default class ItemRepository implements IItemRepository {
   }
 
   private async saveItemImage (itemId: string, pathImages: ItemImage[]): Promise<void> {
-    const query = 'INSERT INTO order_item(id, item_id, base64, storage_path) VALUES($1, $2, $3, $4) ON CONFLICT (id) DO UPDATE SET base64=$3, storage_path=$4 RETURNING *'
+    const query = 'INSERT INTO item_image(id, item_id, base64, storage_path) VALUES($1, $2, $3, $4) ON CONFLICT (id) DO UPDATE SET base64=$3, storage_path=$4 RETURNING *'
     await Promise.all(pathImages.map(async ({ id, base64, storagePath }) => {
       const values = [id, itemId, base64, storagePath]
       await this.connection.query(query, values)
     }))
+  }
+
+  async findById (id: string): Promise<Item | undefined> {
+    const query = `
+    SELECT * FROM "item" i
+    JOIN item_image ii ON ii.item_id = i.id
+    WHERE i.id = $1
+    `
+    const result = await this.connection.query(query, [id])
+    if (result.rows.length === 0) return undefined
+    return result.rows.reduce((acc: Item | undefined, row: ItemRow) => {
+      const { id, name, category, description, price, item_image_id: itemImageId, base64, storage_path: storagePath } = row
+      const itemImage = new ItemImage(itemImageId, id, base64, storagePath)
+      if (acc === undefined) {
+        return new Item(id, name, category, price, description, [itemImage])
+      }
+      return new Item(acc.id, acc.name, acc.category, acc.price, acc.description, [...acc.pathImages, itemImage])
+    }, undefined)
   }
 }
--- src/core/domain/repositories/IItemRepository.ts
+++ src/core/domain/repositories/IItemRepository.ts
@@ -2,4 +2,5 @@ import type Item from '../entities/Item'
 
 export default interface IItemRepository {
   save: (item: Item) => Promise<string>
+  findById: (id: string) => Promise<Item | undefined>
 }
