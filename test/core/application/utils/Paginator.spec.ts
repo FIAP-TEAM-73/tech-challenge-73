82b6dcc8cd4bae4c39995a152365408f1fa63523
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-25 14:37:32 -0300

    test: ensure Paginator return lastPage when is last page

--- test/core/application/utils/Paginator.spec.ts
+++ test/core/application/utils/Paginator.spec.ts
@@ -2,11 +2,15 @@ import Paginator from '../../../../src/core/application/utils/Paginator'
 
 describe('Pagination', () => {
   it('Should return firstPage when page is 0', () => {
-    const sut = new Paginator([], 10, 0, 100)
+    const sut = new Paginator([], 10, 1, 100)
     expect(sut.isFirstPage()).toBe(true)
   })
   it('Should return totalPages 10 when size is 3 and content is 30', () => {
-    const sut = new Paginator(['John', 'Mary', 'Anna'], 3, 0, 30)
+    const sut = new Paginator(['John', 'Mary', 'Anna'], 3, 1, 30)
     expect(sut.getTotalPages()).toBe(10)
+  })
+  it('Should return lastPage when there is no items', () => {
+    const sut = new Paginator(['John'], 10, 1, 1)
+    expect(sut.isLastPage()).toBe(true)
   })
 })
