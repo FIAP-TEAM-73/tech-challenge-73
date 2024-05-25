58c28e1f8344139e4eda0d5d079f0a2710b2be23
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-25 14:26:44 -0300

    test: ensure Paginator return totalPages with the correct value

--- test/core/application/utils/Paginator.spec.ts
+++ test/core/application/utils/Paginator.spec.ts
@@ -2,7 +2,11 @@ import Paginator from '../../../../src/core/application/utils/Paginator'
 
 describe('Pagination', () => {
   it('Should return firstPage when page is 0', () => {
-    const sut = new Paginator([], 0, 100)
+    const sut = new Paginator([], 10, 0, 100)
     expect(sut.isFirstPage()).toBe(true)
+  })
+  it('Should return totalPages 10 when size is 3 and content is 30', () => {
+    const sut = new Paginator(['John', 'Mary', 'Anna'], 3, 0, 30)
+    expect(sut.getTotalPages()).toBe(10)
   })
 })
