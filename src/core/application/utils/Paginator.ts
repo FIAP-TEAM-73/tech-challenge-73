12a04ed9aea25f971cd886f889e46524b0bd1613
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-25 14:37:21 -0300

    feat: ensure Paginator return lastPage when is last page

--- src/core/application/utils/Paginator.ts
+++ src/core/application/utils/Paginator.ts
@@ -7,10 +7,14 @@ export default class Paginator<T> {
   ) {}
 
   isFirstPage (): boolean {
-    return this.page === 0
+    return this.page === 1
   }
 
   getTotalPages (): number {
     return Math.ceil(this.total / this.size)
+  }
+
+  isLastPage (): boolean {
+    return this.getTotalPages() === this.page
   }
 }
