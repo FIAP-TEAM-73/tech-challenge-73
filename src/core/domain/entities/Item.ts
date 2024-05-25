ce8a58b11aa214af9b9f9c77d40a6fce20062546
acdmAnderson <acdm.anderson@hotmail.com>
2024-05-25 09:48:03 -0300

    feat: ensure Item is deactivated correctly

--- src/core/domain/entities/Item.ts
+++ src/core/domain/entities/Item.ts
@@ -10,11 +10,16 @@ export default class Item {
     readonly category: ItemCategory,
     readonly price: number,
     readonly description: string,
-    readonly pathImages: ItemImage[]
+    readonly pathImages: ItemImage[],
+    readonly isActive: boolean = true
   ) {
     assertArgumentLength(name, 3, 100, 'Name must be greater than 3 and less than 100!')
     assertArgumentUnionType(category, Object.values(categories), `Category '${category}' is invalid!`)
     assertArgumentMin(price, 0, 'Price must be greater than 0')
     assertArgumentLength(description, 3, 255, 'Description must be greater than 3 and less than 255!')
+  }
+
+  public deactivate (): Item {
+    return new Item(this.id, this.name, this.category, this.price, this.description, this.pathImages, false)
   }
 }
