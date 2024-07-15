1166356194b98d156389b3f1c81e2c5a976d294d
acdmAnderson <acdm.anderson@hotmail.com>
2024-07-15 08:39:33 -0300

    test: ensure PaymentStatus is not created when status does not exist

--- test/entities/PaymentStatus.spec.ts
+++ test/entities/PaymentStatus.spec.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import PaymentStatus from '../../src/entities/PaymentStatus'
 describe('PaymentStatus', () => {
   it('Should create a PaymentStatus when every attribute is valid', () => {
     expect(() => new PaymentStatus('any_id', 'AWAITING_PAYMENT')).not.toThrow(new Error(''))
+  })
+  it('Should not create a PaymentStatus when status is wrong', () => {
+    const wrongStatus: any = 'WRONG_STATUS'
+    expect(() => new PaymentStatus('any_id', wrongStatus)).toThrow(new Error(`Status '${wrongStatus}' does not exist`))
   })
 })
