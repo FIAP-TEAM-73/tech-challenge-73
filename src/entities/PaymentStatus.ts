f4761790b465967c046366520f718fdf1dee2198
acdmAnderson <acdm.anderson@hotmail.com>
2024-07-15 08:39:13 -0300

    feat: ensure PaymentStatus is not created when status does not exist

--- src/entities/PaymentStatus.ts
+++ src/entities/PaymentStatus.ts
@@ -1,6 +1,10 @@
-const status = ['AWAITING_PAYMENT', 'PAYMENT_ACCEPTED', 'PAYMENT_REFUSED'] as const
-export type PaymentStatuses = (typeof status)[number]
+import { assertArgumentUnionType } from './base/AssertionConcerns'
+
+const statuses = ['AWAITING_PAYMENT', 'PAYMENT_ACCEPTED', 'PAYMENT_REFUSED'] as const
+export type PaymentStatuses = (typeof statuses)[number]
 
 export default class PaymentStatus {
-  constructor (readonly id: string, readonly status: PaymentStatuses) {}
+  constructor (readonly id: string, readonly status: PaymentStatuses) {
+    assertArgumentUnionType(status, Object.values(statuses), `Status '${status}' does not exist`)
+  }
 }
