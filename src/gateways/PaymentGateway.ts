e87be58759c444bae6bc8cdc11594b0343b16f3c
acdmAnderson <acdm.anderson@hotmail.com>
2024-07-16 21:02:21 -0300

    feat: ensure PaymentGateway returns a payment when it exists

--- src/gateways/PaymentGateway.ts
+++ src/gateways/PaymentGateway.ts
@@ -1,7 +1,18 @@
-import type Payment from '../entities/Payment'
-import type PaymentStatus from '../entities/PaymentStatus'
+import Payment from '../entities/Payment'
+import PaymentStatus from '../entities/PaymentStatus'
+import { type PaymentStatuses } from '../entities/PaymentStatus'
 import type IConnection from '../interfaces/IConnection'
 import { type IPaymentGateway } from '../interfaces/IPaymentGateway'
+
+interface PaymentRow {
+  id: string
+  order_id: string
+  value: number
+  integration_id: string
+  qr_code: string
+  payment_status_id: string
+  status: PaymentStatuses
+}
 
 export default class PaymentGateway implements IPaymentGateway {
   constructor (private readonly connection: IConnection) {}
@@ -15,11 +26,29 @@ export default class PaymentGateway implements IPaymentGateway {
     return paymentId
   }
 
-  async savePaymentStatus (paymentId: string, statuses: PaymentStatus[]): Promise<void> {
+  private async savePaymentStatus (paymentId: string, statuses: PaymentStatus[]): Promise<void> {
     const query = 'INSERT INTO "payment_status"(id, payment_id, "status") VALUES($1, $2, $3) ON CONFLICT DO NOTHING;'
     await Promise.all(statuses.map(async ({ id, status }) => {
       const values = [id, paymentId, status]
       await this.connection.query(query, values)
     }))
+  }
+
+  async findById (paymentId: string): Promise<Payment | undefined> {
+    const query = `
+    SELECT p.*, ps.id as payment_status_id, ps.status FROM "payment" p
+    JOIN payment_status ps ON ps.payment_id = p.id
+    WHERE p.id = $1
+    `
+    const result = await this.connection.query(query, [paymentId])
+    if (result.rows.length === 0) return undefined
+    return result.rows.reduce((acc: Payment | undefined, row: PaymentRow) => {
+      const { id, order_id: orderId, integration_id: integrationId, payment_status_id: paymentStatusId, qr_code: qrCode, status, value } = row
+      const paymentStatus = new PaymentStatus(paymentStatusId, status)
+      if (acc === undefined) {
+        return new Payment(id, orderId, value, [paymentStatus], qrCode, integrationId)
+      }
+      return new Payment(acc.id, acc.orderId, acc.value, [...acc.statuses, paymentStatus], acc.qrCode, acc.integrationId)
+    }, undefined)
   }
 }
--- src/interfaces/IPaymentGateway.ts
+++ src/interfaces/IPaymentGateway.ts
@@ -2,4 +2,5 @@ import type Payment from '../entities/Payment'
 
 export interface IPaymentGateway {
   save: (payment: Payment) => Promise<string>
+  findById: (paymentId: string) => Promise<Payment | undefined>
 }
